8798371a6f9cb7bcc800da02b5a3899a22ba3d5e
malroot <malroot@hell.net>
2017-04-04 21:40:03 +0800

    add binutils-conf-2.sh gcc-conf-2.sh lfs-chroot.sh lfs-create-dir.sh libstdc-conf.sh

---
+++ glibc-conf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+../configure                            \
+    --prefix=/tools                     \
+    --host=$LFS_TGT                     \
+    --build=$(../scripts/config.guess)  \
+    --enable-kernel=2.6.32              \
+    --with-headers=/tools/include       \
+    libc_cv_forced_unwind=yes           \
+    libc_cv_c_cleanup=yes
+
